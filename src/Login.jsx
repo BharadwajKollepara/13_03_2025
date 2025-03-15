91516b2e99fc022eee6de5d2e09ae22cdea71197
Bharadwaj Kollepara <138842246+BharadwajKollepara@users.noreply.github.com>
2025-03-15 15:34:57 +0530

    Add files via upload

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,18 +1,36 @@
-import React from "react";
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./login.css";
 
 const Login = () => {
+  const uname = useRef(null);
+  const upwd = useRef(null);
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+
+  const login = () => {
+    if (!uname.current.value || !upwd.current.value) {
+      setError("Username and Password are required");
+      return;
+    }
+
+    if (uname.current.value === "bharadwaj" && upwd.current.value === "12345") {
+      navigate("/dashboard");
+    } else {
+      setError("Invalid Credentials");
+    }
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <label>Username:</label>
-        <input type="text" name="username" />
-        <br />
-        <label>Password:</label>
-        <input type="password" name="password" />
-        <br />
-        <input type="submit" value="Login" />
-      </form>
+    <div className="login-container">
+      <h1>E-cart Application</h1>
+      <div className="login-box">
+        <h2>Login</h2>
+        {error && <p className="error-message">{error}</p>}
+        <input type="text" ref={uname} placeholder="Enter username" />
+        <input type="password" ref={upwd} placeholder="Enter password" />
+        <button onClick={login}>Login</button>
+      </div>
     </div>
   );
 };
